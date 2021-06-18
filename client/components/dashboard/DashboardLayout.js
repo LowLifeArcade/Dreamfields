e759df6e49e4fe730b50dc732f2592a07ad6ec84
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-06-17 22:12:58 -0700

    working on sidebar in dashboard

--- client/components/dashboard/DashboardLayout.js
+++ client/components/dashboard/DashboardLayout.js
@@ -1,16 +1,24 @@
 import DashboardSidebar from './DashboardSidebar';
-
+// TODO: pull class names from global into this file along with media query
+// TODO: media query right side away first at like 1000 then later left side
 const DashboardLayout = ({ title, children, items, ...restProps }) => {
   return (
     <div>
       <h1 className="miniJumboTron">{title}</h1>
       <div className="container">
-        <div className="leftSideContainer">
+        <div className="left-side-container ">
           <DashboardSidebar items={items} />{' '}
         </div>
         <div className="pageContainer">{children}</div>
         <div className="rightSideContainer"></div>
       </div>
+      <style jsx>{`
+        .left-side-container {
+          width: 20vw;
+          top: 0;
+          position: sticky;
+        }
+        `}</style>
     </div>
   );
 };
